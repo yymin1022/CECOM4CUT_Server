e50c6875313b8cdd203473777c40808ccb9aa69f
yymin1022 <yymin1022@gmail.com>
2024-03-06 03:19:18 +0900

    Update index.js : Enabled Dotenv

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
 import http from "http";
+
+dotenv.config();
 
 const app = express();
 const server = http.createServer(app);
