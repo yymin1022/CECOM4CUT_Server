3629f9c2945105fa80fe1cbbd792c10c71ed52aa
yymin1022 <yymin1022@gmail.com>
2024-03-06 03:08:47 +0900

    Update index.js : Added Express Server Basic Code

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-console.log("Hello, World!");
+import express from "express";
+import cors from "cors";
+import http from "http";
+
+const app = express();
+const server = http.createServer(app);
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+    res.send("Hello, World!");
+});
+
+server.listen(8080, "0.0.0.0", () => {
+    console.log("Server listen on port 8080");
+});
