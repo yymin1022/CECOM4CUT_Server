2beaa7fede5ef7f2d788f479ad7bee8e218699aa
yymin1022 <yymin1022@gmail.com>
2024-03-11 03:06:42 +0900

    Update index.js : Applied Date Format for Filename

--- src/index.js
+++ src/index.js
@@ -20,19 +20,27 @@ app.get("/", (req, res) => {
 
 app.get("/download/:filename", (req, res) => {
     const FILE_NAME = req.params["filename"];
-    const FILE_FULL_DIR = `${FILE_DIR}/${FILE_NAME}`;
+    const FILE_FULL_DIR = `${FILE_DIR}/${FILE_NAME}.jpg`;
 
     res.download(FILE_FULL_DIR);
 });
 
 app.post("/upload", (req, res) => {
+    const DATE = new Date();
+    const DATE_MONTH = ('0' + (DATE.getMonth() + 1)).slice(-2);
+    const DATE_DAY = ('0' + DATE.getDate()).slice(-2);
+    const DATE_HOUR = ('0' + DATE.getHours()).slice(-2);
+    const DATE_MINUTE = ('0' + DATE.getMinutes()).slice(-2);
+    const DATE_SECOND = ('0' + DATE.getSeconds()).slice(-2);
+    const CUR_DATE = `${DATE_MONTH}${DATE_DAY}${DATE_HOUR}${DATE_MINUTE}${DATE_SECOND}`;
+
     const FILE_DATA = req.body["file"];
-    const FILE_NAME = "file.jpg";
+    const FILE_NAME = `${CUR_DATE}.jpg`;
 
     const IMAGE_FILE = Buffer.from(FILE_DATA, "base64");
     writeFileSync(`${FILE_DIR}/${FILE_NAME}`, IMAGE_FILE);
 
-    res.send(FILE_NAME);
+    res.send(CUR_DATE);
 });
 
 server.listen(8080, "0.0.0.0", () => {
